62abf0ccfa6012b6df23a5cf199b94f0fec73a48
mattgerard <matt@matthewgerard.com>
2018-08-22 02:28:52 -0700

    remove worker update ga

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-90649111-2');
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
