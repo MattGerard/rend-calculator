07cc7f74aa47341b677b082a53746175508ecb76
mattgerard <matt@matthewgerard.com>
2018-08-22 02:22:18 -0700

    update for dep

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Calculator from "./Calculator";
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Calculator';
+import styled, {ThemeProvider} from 'styled-components';
+import theme from 'styled-theming';
+
+const boxBackgroundColor = theme('mode', {
+  light: '#c6b5a6',
+  dark: '#2d1a19',
+});
+
+const textColor = theme('mode', {
+  light: '#2d1a19',
+  dark: '#c6b5a6',
+});
+
+const Box = styled.div`
+  background-color: ${boxBackgroundColor};
+  color: ${textColor};
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 20px;
+`;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mode: 'dark',
+    };
+  }
   render() {
+    const {mode} = this.state;
     return (
-      <React.Fragment>
-        <h1 className="center">Rend Research Calculator</h1>
-        <Calculator />
-      </React.Fragment>
+      <ThemeProvider theme={{mode: mode}}>
+        <Box>
+          <button onClick={this.toggleMode}>Toggle Mode</button>
+          <React.Fragment>
+            <h1 className="center">Rend Research Calculator</h1>
+            <Calculator />
+          </React.Fragment>
+        </Box>
+      </ThemeProvider>
     );
   }
+  toggleMode = () => {
+    const {mode} = this.state;
+
+    this.setState({mode: mode === 'dark' ? 'light' : 'dark'});
+  };
 }
 
 export default App;
--- src/Calculator/index.js
+++ src/Calculator/index.js
@@ -1,26 +1,58 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import MysticismIcon from '../assets/images/Mysticism.png';
+import ConstructionIcon from '../assets/images/Construction.png';
+import AdventureIcon from '../assets/images/Adventuring.png';
+import InventionIcon from '../assets/images/Invention.png';
+import Styled from 'styled-components';
+
+// setter
+// localStorage.setItem('myData', data);
+
+// // getter
+// localStorage.getItem('myData');
+
+// remove
+// localStorage.removeItem(key);
+
+const IconWrap = Styled.div`
+    border: 2px solid transparent;
+    background-clip: padding-box,border-box;
+    background-origin: padding-box,border-box;
+    background-image: linear-gradient(#fff,#fff),linear-gradient(#86653c,#8e342c);
+    display: inline-block;
+    width: 48px;
+    height: 48px;
+`;
+
+const ResearchHeading = Styled.div`
+  text-align: center;
+`;
 
 const DefaultResearchSettings = [
   {
-    name: "Mystic",
+    name: 'Mysticism',
     requiredSparks: 5,
-    percentPerResearch: 1
+    percentPerResearch: 1,
+    icon: MysticismIcon,
   },
   {
-    name: "Construction",
+    name: 'Construction',
     requiredSparks: 10,
-    percentPerResearch: 1
+    percentPerResearch: 1,
+    icon: ConstructionIcon,
   },
   {
-    name: "Adventure",
+    name: 'Adventuring',
     requiredSparks: 5,
-    percentPerResearch: 1
+    percentPerResearch: 1,
+    icon: AdventureIcon,
   },
   {
-    name: "Invention",
+    name: 'Invention',
     requiredSparks: 8,
-    percentPerResearch: 1
-  }
+    percentPerResearch: 1,
+    icon: InventionIcon,
+  },
 ];
 
 export default class Calculator extends Component {
@@ -31,8 +63,8 @@ export default class Calculator extends Component {
       currentPercent: 0,
       selectedResearch: 0,
       sparksNeeded: null,
-      workItemName: "",
-      workItems: []
+      workItemName: '',
+      workItems: [],
     };
   }
 
@@ -43,9 +75,9 @@ export default class Calculator extends Component {
       selectedResearch,
       sparksNeeded,
       workItemName,
-      workItems
+      workItems,
     } = this.state;
-    console.log(research, "re");
+
     return (
       <div className="workspace">
         {workItems.length ? (
@@ -65,8 +97,12 @@ export default class Calculator extends Component {
             {research.map((research, i) => {
               return (
                 <div key={i}>
-                  <h3>{research.name} Settings</h3>
-
+                  <ResearchHeading>
+                    <IconWrap>
+                      <img src={research.icon} alt="Logo" />
+                    </IconWrap>
+                    <h3>{research.name}</h3>
+                  </ResearchHeading>
                   <div>
                     <label>Required Sparks Per Research</label>
                     <div className="field">
@@ -75,13 +111,7 @@ export default class Calculator extends Component {
                         name={`${research.name} requiredSparks`}
                         min="0"
                         value={research.requiredSparks}
-                        onChange={e =>
-                          this.updateResearchField(
-                            e,
-                            research.name,
-                            "requiredSparks"
-                          )
-                        }
+                        onChange={e => this.updateResearchField(e, research.name, 'requiredSparks')}
                       />
                     </div>
                   </div>
@@ -94,11 +124,7 @@ export default class Calculator extends Component {
                         min="0"
                         value={research.percentPerResearch}
                         onChange={e =>
-                          this.updateResearchField(
-                            e,
-                            research.name,
-                            "percentPerResearch"
-                          )
+                          this.updateResearchField(e, research.name, 'percentPerResearch')
                         }
                       />
                     </div>
@@ -111,10 +137,7 @@ export default class Calculator extends Component {
           <hr />
           <h3>Select Your Research Type</h3>
           <div className="field">
-            <select
-              value={selectedResearch}
-              onChange={e => this.selectResearch(e)}
-            >
+            <select value={selectedResearch} onChange={e => this.selectResearch(e)}>
               {research.map((research, i) => {
                 return (
                   <option key={i} value={i}>
@@ -166,61 +189,47 @@ export default class Calculator extends Component {
     );
   }
   updateResearchField = (e, typeOfResearch, field) => {
-    console.log(e.target.value, e.target.name, e.target.type, "whats in here?");
-    const { research } = this.state;
+    const {research} = this.state;
     const cloneResearch = [...research];
-    console.log(cloneResearch, "test");
-    const researchToEdit = cloneResearch.find(
-      research => research.name === typeOfResearch
-    );
-    console.log(researchToEdit);
 
-    researchToEdit[field] =
-      e.target.type === "number" ? Number(e.target.value) : e.target.value;
+    const researchToEdit = cloneResearch.find(research => research.name === typeOfResearch);
 
-    console.log({ researchToEdit }, { cloneResearch });
+    researchToEdit[field] = e.target.type === 'number' ? Number(e.target.value) : e.target.value;
 
-    this.setState({ research: cloneResearch });
+    this.setState({research: cloneResearch, currentPercent: 0, sparksNeeded: null});
   };
 
   updatePercentField = e => {
-    const { selectedResearch, research } = this.state;
-    console.log(e.target.value, e.target.name, e.target.type, "whats in here?");
+    const {selectedResearch, research} = this.state;
 
     const currentResearch = research[selectedResearch];
 
     const currentPercentage = e.target.value;
 
-    const sparksNeeded = this.getSparksNeeded(
-      currentPercentage,
-      currentResearch
-    );
+    const sparksNeeded = this.getSparksNeeded(currentPercentage, currentResearch);
 
-    this.setState({ [e.target.name]: Number(e.target.value), sparksNeeded });
+    this.setState({[e.target.name]: Number(e.target.value), sparksNeeded});
   };
 
   updateField = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({[e.target.name]: e.target.value});
   };
 
   selectResearch = e => {
-    this.setState({ selectedResearch: Number(e.target.value) });
+    this.setState({selectedResearch: Number(e.target.value)});
   };
 
   getSparksNeeded(currentPercentage, currentResearch) {
     const missingPercent = 100 - currentPercentage;
-    const percentPerMissing =
-      missingPercent / currentResearch.percentPerResearch;
-    const sparksNeeded = Number(
-      percentPerMissing * currentResearch.requiredSparks
-    ).toFixed(1);
+    const percentPerMissing = missingPercent / currentResearch.percentPerResearch;
+    const sparksNeeded = Math.ceil(percentPerMissing * currentResearch.requiredSparks);
     return sparksNeeded;
   }
 
   saveWorkItem = () => {
-    const { sparksNeeded, workItemName, workItems } = this.state;
-    const workItem = { name: workItemName, amount: sparksNeeded };
+    const {sparksNeeded, workItemName, workItems} = this.state;
+    const workItem = {name: workItemName, amount: sparksNeeded};
     const cloneWorkItems = [...workItems, workItem];
-    this.setState({ workItems: cloneWorkItems });
+    this.setState({workItems: cloneWorkItems});
   };
 }
